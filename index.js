0673fbd9d427da23f1509f200d198b34217b060a
Kai <44775276+xyverious@users.noreply.github.com>
2022-04-26 07:32:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-client.on('ready', () => {
-    console.log('pret !')
-    setInterval(changeColor, config.speed)
-    client.user.setActivity("%help", {
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("redi!")
+    client.user.setActivity("Dia Bersamanya", {
         type: "WATCHING"
     });
 });
+
+client.login("NDkxMDU0MTMyNTQzNzUwMTQ2.YmctkA.b4tktWxyDJvjKYlE-EcRrwJwacc");
