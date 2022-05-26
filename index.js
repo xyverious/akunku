190bb5282dcf5153eef2966cc4abf30acc800cae
Kai <44775276+xyverious@users.noreply.github.com>
2022-05-26 10:52:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const Eris = require('eris');
-const bot = new Eris()
+const Eris = require("eris");
 
-bot.connect("NzM5Mzk5NTk3NTU3ODA5MjEy.Gx8yOp.t2P8_oUV6Q3r8LA0-pD6wMzTlanA_T7AQKZsu8");
+// Replace TOKEN with your bot account's token
+const bot = new Eris("NzM5Mzk5NTk3NTU3ODA5MjEy.Gx8yOp.t2P8_oUV6Q3r8LA0-pD6wMzTlanA_T7AQKZsu8");
+
+bot.on("ready", () => { // When the bot is ready
+    console.log("Ready!"); // Log "Ready!"
+});
+
+bot.connect()
