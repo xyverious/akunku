b0b5a6e799f71faa0acafc6169601a6670ae941e
Kai <44775276+xyverious@users.noreply.github.com>
2022-05-26 10:46:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Eris = require('eris');
+const bot = new Eris()
 
-client.on("ready", () => {
-    console.log("redi!")
-    client.user.setActivity("Dia Bersamanya", {
-        type: "WATCHING"
-    });
-});
-
-client.login("NzM5Mzk5NTk3NTU3ODA5MjEy.Gx8yOp.t2P8_oUV6Q3r8LA0-pD6wMzTlanA_T7AQKZsu8");
+bot.connect("NzM5Mzk5NTk3NTU3ODA5MjEy.Gx8yOp.t2P8_oUV6Q3r8LA0-pD6wMzTlanA_T7AQKZsu8");
